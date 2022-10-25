ffd743594054211b2dd34e286535aa8fa3871a4b
EvelinVillamil5 <e.villamilg@uniandes.edu.co>
2022-10-25 14:03:31 -0500

    Entrega Final

--- Scripts/main.js
+++ Scripts/main.js
@@ -19,5 +19,24 @@ function insertSeasonsAvrg(series) {
     var html = '<p> &nbsp; Seasons average: ' + avrg + '</p>';
     table.insertAdjacentHTML('afterend', html);
 }
+function showDetail(series) {
+    var trs = document.querySelectorAll('tr.fila-serie');
+    var col = document.getElementById('detail-col');
+    // Detalle por defecto: Breaking Bad
+    var html = "\n        <div class=\"card\" style=\"width: 18rem;\">\n            <img class=\"card-img-top\" src=".concat(series[0].cover, ">\n            <div class=\"card-body\">\n                <h5 class=\"card-title\">").concat(series[0].name, "</h5>\n                <p class=\"card-text\">\n                    ").concat(series[0].description, "<br><br>\n                    <a href=").concat(series[0].link, ">").concat(series[0].link, "</a>\n                    <br><br>\n                </p>\n            </div>\n        </div>");
+    col.innerHTML = html;
+    var _loop_1 = function (i) {
+        trs[i].addEventListener('click', function () {
+            console.log('Fila ' + i + ' recibe click');
+            var html = "\n                <div class=\"card\" style=\"width: 18rem;\">\n                    <img class=\"card-img-top\" src=".concat(series[i].cover, ">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title\">").concat(series[i].name, "</h5>\n                        <p class=\"card-text\">\n                            ").concat(series[i].description, "<br><br>\n                            <a href=").concat(series[i].link, ">").concat(series[i].link, "</a>\n                            <br><br>\n                        </p>\n                    </div>\n                </div>");
+            col.innerHTML = html;
+        });
+    };
+    // Hacer click en cada fila muestra su detalle
+    for (var i = 0; i < trs.length; i++) {
+        _loop_1(i);
+    }
+}
 insertSeries(series);
 insertSeasonsAvrg(series);
+showDetail(series);
--- main.ts
+++ main.ts
@@ -2,10 +2,11 @@ import { Serie } from './serie.js';
 import { series } from './data.js';
 
 function insertSeries(series: Serie[]): void {
-    let tbody: HTMLElement = document.getElementById('series')!;
+    const tbody: HTMLElement = document.getElementById('series')!;
     console.log('Desplegando series.');
     series.map(s => {
         let tr: HTMLElement = document.createElement('tr');
+        tr.setAttribute('class', 'fila-serie');
         let html: string = `<td> <b>${s.id}</b> </td>
                               <td> <a href=${s.link} target="_blank">${s.name}</a> </td>
                               <td> ${s.channel} </td>
@@ -21,10 +22,49 @@ function insertSeasonsAvrg(series: Serie[]): void {
     series.map(s => {sum += s.seasons});
     const avrg: number = sum / series.length;
     console.log('Desplegando promedio.')
-    let table: HTMLElement = document.getElementById('tabla-series')!;
+    const table: HTMLElement = document.getElementById('tabla-series')!;
     const html: string = '<p> &nbsp; Seasons average: '+avrg+'</p>'
     table.insertAdjacentHTML('afterend', html)
 }
 
+function showDetail(series: Serie[]): void {
+    const trs: NodeList = document.querySelectorAll('tr.fila-serie');
+    const col: HTMLElement = document.getElementById('detail-col')!;
+
+    const html: string = `
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src=${series[0].cover}>
+            <div class="card-body">
+                <h5 class="card-title">${series[0].name}</h5>
+                <p class="card-text">
+                    ${series[0].description}<br><br>
+                    <a href=${series[0].link}>${series[0].link}</a>
+                    <br><br>
+                </p>
+            </div>
+        </div>`
+    col.innerHTML = html;
+
+    for(let i: number = 0; i < trs.length; i++) {
+        trs[i].addEventListener('click', function() {
+            console.log('Fila '+i+' recibe click');
+            const html: string = `
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src=${series[i].cover}>
+                    <div class="card-body">
+                        <h5 class="card-title">${series[i].name}</h5>
+                        <p class="card-text">
+                            ${series[i].description}<br><br>
+                            <a href=${series[i].link}>${series[i].link}</a>
+                            <br><br>
+                        </p>
+                    </div>
+                </div>`
+            col.innerHTML = html;
+        });
+    }
+}
+
 insertSeries(series);
 insertSeasonsAvrg(series);
+showDetail(series);
